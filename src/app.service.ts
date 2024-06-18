241988672e6d22d808c9a97f7c9727c1784122e9
Moula_Saginala <saginalamoula1635@gmail.com>
2024-06-18 16:42:23 +0530

    Changed Servcice.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!Good morning, This is Moula';
+    return 'Hello World!good morning, This is Moula';
   }
 }
